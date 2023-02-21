0c32dc3069fd26cfebe78bdfe91b996dc928fc53
Eduardo Toro <etoromol@gmail.com>
2023-02-21 00:50:54 -0600

    mockup 2

--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("\n   -------------------- ")
-	fmt.Println(" ( Hellow, World!       )")
-	fmt.Println(" ( Version  > 1.0.0     )")
-	fmt.Println(" ( Hostname > _host     )")
-	fmt.Println("   -------------------- ")
+	fmt.Println("\n     ----------------- ")
+	fmt.Println("   ( Hellow, World!    )")
+	fmt.Println("  (  Version  > 1.0.0   )")
+	fmt.Println(" (   Hostname > _host    )")
+	fmt.Println("   --------------------- ")
         fmt.Println("        \\   ^__^ ")
         fmt.Println("         \\  (oo)\\_______ ")
         fmt.Println("            (__)\\       )\\/\\ ")
