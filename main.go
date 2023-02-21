04090b7cc6818edf023a105bc6e4452a93afd6a6
Eduardo Toro <etoromol@gmail.com>
2023-02-21 00:33:26 -0600

    Initial mockup for the main banner

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("\n   -------------------- ")
+	fmt.Println(" ( Hellow, World!       )")
+	fmt.Println(" ( Version  > 1.0.0     )")
+	fmt.Println(" ( Hostname > _host     )")
+	fmt.Println("   -------------------- ")
+        fmt.Println("        \\   ^__^ ")
+        fmt.Println("         \\  (oo)\\_______ ")
+        fmt.Println("            (__)\\       )\\/\\ ")
+        fmt.Println("                ||----w | ")
+        fmt.Println("                ||     || ")
+}
